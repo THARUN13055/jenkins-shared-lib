80a286e754bb444a33f83898a50cfa4424581270
THARUN13055 <megatharun13055>
2023-05-05 20:15:40 +0530

    all

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,5 +3,6 @@ def call(String project, String imageTag, String hubUser){
     withCredentials([usernamePassword(credentialsId: 'tharun', passwordVariable: 'pass', usernameVariable: 'user')]) {
       sh "docker login -u '$user' -p '$pass'"
     }
+    sh "docker push ${hubUser}/${project}:${imageTag}"
 
 }
