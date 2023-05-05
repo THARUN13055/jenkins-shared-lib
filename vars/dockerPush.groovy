2dd6cb0051eb2c6d6da5cf74a914606d84ad0ffb
THARUN13055 <megatharun13055>
2023-05-05 19:28:44 +0530

    all

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,4 +1,6 @@
 def call(String project, String imageTag, String hubUser){
-    sh "    docker image build -t ${hubUser}/${project}:${imageTag} .
-"
+  sh """
+    docker image build -t ${hubUser}/${project}:${imageTag} .
+    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+  """
 }
